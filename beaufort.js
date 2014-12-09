0787f9449f8f8dc20d73fd5416df21947210c4bf
Jay Morrow <takuya813@gmail.com>
2014-12-09 22:48:28 +1300

    Cleaned up codebase

--- beaufort.js
+++ beaufort.js
@@ -1,7 +1,3 @@
-/**
- * kmh [0,5.5,11,19,28,38,49,61,74,88,102,117,153,177,209,249,332,418,512]
- */
-
 var windDesc = [
                "calm", 
                "light air", 
@@ -14,54 +10,56 @@ var windDesc = [
                "gale",
                "strong gale",
                "storm",
-               "violent storm",
-               "category one hurricane",
-               "category two hurricane",
-               "category three hurricane",
-               "category four hurricane",
-               "category five hurricane"
+               "violent storm"
                ];
 
-var kmhLimits = [1,7,12,20,31,40,51,62,75,88,103,118,154,178,210,250,333,419];
+// each array element is the max windspeed in kmh for that beaufort 
+// number (starting with 1)
+// http://about.metservice.com/assets/downloads/learning/winds_poster_web.pdf
+var kmhLimits = [1,6,11,19,30,39,50,61,74,87,102,117,177,249,332,418];
 
 // input is kmh, output is beaufort number
 // initialValue is 0 so we can safely add to it to get the reduce.
+// TODO add code for (E)Fujita EF0-EF5, and Saffir-Simpson
+// EF0: 8-11
+// EF1: 12 
+//  ...
+// EF5: 16
+// SafSim: Cat1/2: 12
+//         Cat3/4: 13
+//         Cat5  : 14
+//
 numberFromKmh = function(kmh) {
-  return kmhLimits.reduce(function(previousValue, currentValue, index, array) {
+  var beauNum = kmhLimits.reduce(function(previousValue, currentValue, index, array) {
     return previousValue + (kmh > currentValue ? 1 : 0);
   },0);
+
+  return beauNum;
 }
 
-numberFromKmhFilter = function(kmh) {
-  // speedSegments is an array of each beaufort number's max speed
-  // for which the kmh passed in is less. The count of this array
-  // is the beaufort number.
-  //
-  var speedSegments = kmhLimits.filter(function(element) {
-    return kmh > element;
-  });
-
-  return speedSegments.length;
+numberFromMps = function(mps) {
+  var kmh = mps * 3.6; // scale factor, 1km/1000m * 3600s/hr
+  return numberFromKmh(kmh);
 }
 
 descFromKmh = function(kmh) {
   return descFromNumber(numberFromKmh(kmh));
 }
 
-descFromKmh2 = function(kmh) {
-  return descFromNumber(numberFromKmhFilter(kmh));
-}
-
 descFromMps = function(mps) {
   // convert to kmh because we already defined the kmh scale
-  var kmh = mps * (1/1000) * 60 * 60;
+  var kmh = mps * 3.6;
   return descFromKmh(kmh);
 }
 
 descFromNumber = function(beauNum) {
+  if(beauNum > windDesc.length) return "";
+
   return windDesc[beauNum];
 }
 
 exports.descFromKmh = descFromKmh;
-exports.descFromKmh2 = descFromKmh2;
 exports.numberFromKmh = numberFromKmh;
+exports.descFromMps = descFromMps;
+exports.numberFromMps = numberFromMps;
+
